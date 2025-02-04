2cdbd2f542f42e05f460e9e159d30b5807200e71
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-02-04 19:53:11 +0000

    fix lints

--- R/functions/get_goodreads_cover_df.R
+++ R/functions/get_goodreads_cover_df.R
@@ -1,8 +1,8 @@
 get_goodreads_cover_df <- function(.data) {
   goodreads_url <- paste("https://www.goodreads.com/book/show/", .data$`Book Id`, sep = "")
-  
+
   goodreads_page <- read_html(goodreads_url)
-  
+
   goodreads_cover <- goodreads_page %>%
     html_element(css = "div.BookCover img") %>%
     html_attr("src")
