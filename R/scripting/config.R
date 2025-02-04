d541d428802b399d04237ce14e46340f48b0c7d3
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-02-04 19:15:24 +0000

    add global variables and make sections explicit

--- R/scripting/config.R
+++ R/scripting/config.R
@@ -2,8 +2,13 @@ library(epubr)
 library(tidyverse)
 library(rvest)
 
+# Source functions -------------------------------------------------------------
 source("R/functions/source_functions.R")
 source_functions()
 
+# Set Options ------------------------------------------------------------------
 options(log_verbose = TRUE)
 options(r_functions_path = "R/functions")
+
+# Set Global Variables ---------------------------------------------------------
+goodreads_file_path <- "data/input/goodreads_library_export.csv"
