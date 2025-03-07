ebd06f16c493f576b396c2026ed4dab15a46f440
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-07 17:01:48 +0000

    change container and data object for currently reading

--- R/functions/ui_novels_currently_reading.R
+++ R/functions/ui_novels_currently_reading.R
@@ -1,9 +1,8 @@
-ui_novels_currently_reading <- function(.novels) {
+ui_novels_currently_reading <- function(currently_reading) {
   renderUI({
-    currently_reading <- .novels %>% filter(Bookshelves == "currently-reading")
     if (nrow(currently_reading) > 0) {
       lapply(seq_len(nrow(currently_reading)), function(i) {
-        generate_book_container(currently_reading[i, ], i)
+        generate_novel_container(currently_reading[i, ], i)
       })
     } else {
       p("No books currently being read.")
