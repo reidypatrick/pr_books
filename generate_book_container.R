eb1ffc8a25a6fffc65af1f5c3d021c0e0dd60137
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-07 23:49:02 +0000

    make book conatiner w/o progress/input

---
+++ generate_book_container.R
@@ -0,0 +1,30 @@
+generate_book_container <- function(book, index) {
+  div(
+    class = "book-container",
+    fluidRow(
+      column(
+        4,
+        div(
+          class = "book-cover",
+          img(
+            src = book$Cover_URL,
+            alt = "Book Cover",
+            height = "200px",
+            width = "auto"
+          )
+        )
+      ),
+      column(
+        8,
+        div(
+          class = "book-details",
+          h4(class = "book-title", book$Title),
+          p(class = "book-author", paste("Author:", book$Author)),
+          p(class = "book-year", paste("Year of Publication:", book$Original.Publication.Year)),
+          p(paste("Total Pages:", book$Number.of.Pages)),
+          )
+        )
+      )
+    )
+  )
+}
