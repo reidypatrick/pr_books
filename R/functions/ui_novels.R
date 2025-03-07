1c2a9be7df308f66193d590c9e1e14534e47271f
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-07 17:37:23 +0000

    replace toggle buttons with tabs

--- R/functions/ui_novels.R
+++ R/functions/ui_novels.R
@@ -2,13 +2,27 @@ ui_novels <- function() {
   tabPanel(
     "Novels",
     h3("Novels"),
-    actionButton("toggle_currently_reading", "Currently Reading"),
-    hidden(div(id = "currently_reading_section", uiOutput("currently_reading_ui"))),
-    actionButton("toggle_read", "Read"),
-    hidden(div(id = "read_section", uiOutput("read_ui"))),
-    actionButton("toggle_want_to_read", "Want to Read"),
-    hidden(div(id = "want_to_read_section", uiOutput("want_to_read_ui"))),
-    actionButton("toggle_did_not_finish", "Did Not Finish"),
-    hidden(div(id = "did_not_finish_section", uiOutput("did_not_finish_ui")))
+    tabsetPanel(
+      tabPanel(
+        "Currently Reading",
+        h3("Currently Reading"),
+        div(id = "currently_reading_section",uiOutput("currently_reading_ui"))
+        ),
+      tabPanel(
+        "Read",
+        h3("Read"),
+        div(id = "read_section",uiOutput("read_ui"))
+      ),
+      tabPanel(
+        "Want to Read",
+        h3("Want to Read"),
+        div(id = "want_to_readsection",uiOutput("want_to_read_ui"))
+      ),
+      tabPanel(
+        "Did Not Finish",
+        h3("Did Not Finish"),
+        div(id = "did_not_finish_section",uiOutput("did_not_finish_ui"))
+      )
+    )
   )
 }
