86308b9566f6457aabbf7f8af47694b42fd1e862
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-10 21:20:58 +0000

    add cache capability

--- R/functions/get_goodreads_data.R
+++ R/functions/get_goodreads_data.R
@@ -1,6 +1,10 @@
-get_goodreads_data <- function() {
-  read_csv(goodreads_file_path) %>%
-    mutate(Current.Page = rep(0, nrow(.))) %>%
-    relocate(Current.Page, .after = Number.of.Pages) %>% 
-    mutate(Bookshelves = ifelse(is.na(Bookshelves), "read", Bookshelves))
+get_goodreads_data <- function(use_cache = FALSE) {
+  if (use_cache & exists("cache")) {
+    goodreads_data <- cache
+  } else {
+    read_csv(goodreads_file_path) %>%
+      mutate(Current.Page = rep(0, nrow(.))) %>%
+      relocate(Current.Page, .after = Number.of.Pages) %>%
+      mutate(Bookshelves = ifelse(is.na(Bookshelves), "read", Bookshelves))
+  }
 }
--- app.R
+++ app.R
@@ -2,7 +2,7 @@ source("R/scripting/config.R")
 source("R/scripting/custom_css.R")
 
 # Load Goodreads data -------------------------------------------------------------------------------------------------
-goodreads_data <- get_goodreads_data()
+goodreads_data <- get_goodreads_data(use_cache = FALSE)
 
 # UI ------------------------------------------------------------------------------------------------------------------
 
@@ -39,14 +39,16 @@ server <- function(input, output, session) {
         data <- reactive_data()
         data$Bookshelves[i] <- input[[paste0("shelf_", data$Book.Id[i])]]
         reactive_data(data)
-        
+
         output$currently_reading_ui <- render_ui_currently_reading(data)
         output$want_to_read_ui <- render_ui_want_to_read(data)
         output$read_ui <- render_ui_novels_read(data)
         output$did_not_finish_ui <- render_ui_did_not_finish(data)
+
+        cache <<- data
       })
     })
-    
+
     #### Observe Novel Progress Bars -----------------------------------------------------------------------------------
     lapply(seq_along(data$Book.Id), function(i) {
       observeEvent(input[[paste0("current_page_", data$Book.Id[i])]], {
@@ -54,13 +56,10 @@ server <- function(input, output, session) {
         data$Current.Page[i] <- input[[paste0("current_page_", data$Book.Id[i])]]
         reactive_data(data)
         output[[paste0("reading_progress_", data$Book.Id[i])]] <- render_progress_bar(data[i, ])
+        cache <<- data
       })
     })
   })
-  
-
-
-
 
   ## Poetry Section ---------------------------------------------------------------------------------------------------
   output$poetry_ui <- render_poetry_ui(poetry)
@@ -74,6 +73,7 @@ server <- function(input, output, session) {
   ## Non-Fiction Section ----------------------------------------------------------------------------------------------
   output$non_fiction_ui <- render_non_fiction_ui(non_fiction)
 }
+
 
 
 # Run App -------------------------------------------------------------------------------------------------------------
