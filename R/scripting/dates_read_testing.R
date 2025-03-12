c2312195f05e0b367640ee90414c18b0a2efd112
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-12 17:52:27 +0000

    test dates read dataframe handling

---
+++ R/scripting/dates_read_testing.R
@@ -0,0 +1,7 @@
+activity_data <- data.frame(
+  book_id = c(1953, 12505),
+  date =   c(as.Date("2025-03-11"), as.Date("2025-03-11")),
+  no_of_pages = c(44, 16)
+)
+
+write_csv(activity_data, "data/output/activity_data.csv")
