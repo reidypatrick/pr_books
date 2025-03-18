e84ac14f39450cd7b5bed0685cbc8bb449d920a0
reidypatrick0@gmail.com <reidypatrick0@gmail.com>
2025-03-18 16:25:30 +0000

    implement some activity functions

--- R/functions/render_activity_grid.R
+++ R/functions/render_activity_grid.R
@@ -1,6 +1,33 @@
-render_activity_grid <- function(data) {
+render_activity_grid <- function(book, activity) {
   renderUI({
     # Group data by week
+    book_activity <- filter(activity, book_id == book$Book.Id) %>%
+      group_by(date) %>%
+      summarise(no_of_pages = sum(no_of_pages))
+
+    dates <- seq(Sys.Date() - days(182), Sys.Date(), by = "day")
+
+
+    book_data <- as.data.frame(dates) %>%
+      left_join(book_activity, join_by(x$dates == y$date)) %>%
+      mutate(no_of_pages = ifelse(is.na(no_of_pages), 0, no_of_pages)) %>%
+      mutate( # get weeks from today
+        week = as.numeric(floor(difftime(today(), dates, units = "weeks")))
+      ) %>%
+      mutate( # set day as factor
+        day = factor(
+          weekdays(dates),
+          levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+        )
+      ) %>%
+      mutate( # account for day of week
+        week = ifelse(
+          as.numeric(day) > which(levels(day) == weekdays(today())),
+          week + 1,
+          week
+        )
+      )
+
     weeks <- split(data, data$week)
 
     # Create y-axis labels for Mon, Wed, Fri
@@ -33,21 +60,21 @@ render_activity_grid <- function(data) {
               day_data <- week_data[week_data$day == day, ]
               # Create a cell for the day (if data exists)
               if (nrow(day_data) > 0) {
-                value <- day_data$value[1]
+                no_of_pages <- day_data$no_of_pages[1]
                 date <- day_data$date[1]
-                # Map value to a color gradient
+                # Map no_of_pages to a color gradient
                 color <- rgb(
                   red = 33,
-                  green = min(100 + (value * 5), 255), # Increase green based on value
+                  green = min(100 + (no_of_pages * 5), 255), # Increase green based on no_of_pages
                   blue = 57,
                   maxColorValue = 255,
-                  alpha = value * 25 # Adjust alpha based on value
+                  alpha = min(no_of_pages * 25, 255) # Adjust alpha based on no_of_pages
                 )
-                # Add a tooltip with the date and value
+                # Add a tooltip with the date and no_of_pages
                 div(
                   class = "activity-cell",
                   style = paste0("background-color: ", color, ";"),
-                  title = paste("Date:", date, "\nValue:", value) # Tooltip content
+                  title = paste("Date:", date, "\nPages:", no_of_pages) # Tooltip content
                 )
               } else {
                 # Add an empty cell if no data exists for this day in the week
